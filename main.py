53257443648c03c0564081936effdad9501d1eef
Umesh <urautgol20it@student.mes.ac.in>
2023-07-01 19:25:13 +0530

    day22:render the app titles into columns

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas
 st.set_page_config(layout="wide")
 
 col1,col2 =st.columns(2)
@@ -17,3 +17,16 @@ with col2:
 content2=""" below u will find apps created by me feel free to contact me !!"""
 
 st.write(content2)
+
+
+col3,col4 =st.columns(2)
+
+df=pandas.read_csv("data.csv",sep=";")
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])

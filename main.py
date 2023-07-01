82d67fb2ff20f93dadf830d559bd89c5b6ef1ac3
Umesh <urautgol20it@student.mes.ac.in>
2023-07-01 11:43:12 +0530

    create columns,images,description

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 col1,col2 =st.columns(2)
 
 with col1:
-    st.image("image/photo.png")
+    st.image("images/photo.png",width=600)
 
 with col2:
     st.title("Umesh Rautgol")
     content="""i am from maharashtra solapur"""
-    st.write()
+    st.info(content)

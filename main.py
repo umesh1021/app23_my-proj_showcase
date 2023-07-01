adac5570e23077c84b7654bb53275c8a999b9b00
Umesh <urautgol20it@student.mes.ac.in>
2023-07-01 11:54:43 +0530

    add description on the new columns

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ st.set_page_config(layout="wide")
 
 col1,col2 =st.columns(2)
 
+
 with col1:
     st.image("images/photo.png",width=600)
 
@@ -11,3 +12,8 @@ with col2:
     st.title("Umesh Rautgol")
     content="""i am from maharashtra solapur"""
     st.info(content)
+
+
+content2=""" below u will find apps created by me feel free to contact me !!"""
+
+st.write(content2)
